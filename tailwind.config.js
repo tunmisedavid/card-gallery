fa2a13abd32489363403626be6d92e023cdfeaf1
Arowolo Tunmise <tunmisearowolo094@gmail.com>
2024-10-24 19:33:01 +0100

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          light: "#7f9cf5",
+          DEFAULT: "#3f51b5",
+          dark: "#1c3a73",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
